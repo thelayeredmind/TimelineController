cbdf90de965bbd623491ddbfbd74528961e5db53
CX Dev <cxstudio.development@gmail.com>
2026-06-04 16:21:15 +0200

    remove: Outdated Assets folder that runs stale scripts

--- Assets/TimelineController/ShowAsReadOnlyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-namespace TLM.TimelineController
-{
-    [AttributeUsage(AttributeTargets.Field)]
-    public class ShowAsReadOnlyAttribute : PropertyAttribute { }
-}
-
